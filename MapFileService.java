7db29253f2dfc08e4e0b552b23d296cb0d92c8a9
Jananadi Wedagedara <jananadi92@gmail.com>
2020-03-05 00:05:45 +0900

    Update MapFileService.java

--- MapFileService.java
+++ MapFileService.java
@@ -1,11 +1,11 @@
-package com.cornerstonestech.service;
+package com.cofile.service;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import com.cornerstonestech.common.ReturnCode;
-import com.cornerstonestech.dao.MapFileMapper;
-import com.cornerstonestech.model.ReturnMsg;
-import com.cornerstonestech.model.MapFileModel;
+import com.cofile.common.ReturnCode;
+import com.cofile.dao.MapFileMapper;
+import com.cofile.model.ReturnMsg;
+import com.cofile.model.MapFileModel;
 
 
 public class MapFileService {
@@ -13,63 +13,45 @@ public class MapFileService {
 	@Autowired
 	MapFileMapper mapFileMapper;
 
-
-	
-	//insert files
+	//upload files
 	public ReturnMsg storeMapFile(MapFileModel model) {
-		
 		ReturnMsg returnMsg = new ReturnMsg();
 
         	try {
-        		
         		int siteid = mapFileMapper.getMapFileId(model.getSiteId());
         		if (siteid != 0){
-        					
         			mapFileMapper.updateMapFile(model);
         		}
-                 	
-        		else {
+            else {
         			mapFileMapper.insertMapFile(model);
         		}   	    	
-    		returnMsg.setReturnCode(ReturnCode.OK);
-   	    }
-    	catch (Exception ex) {
-    		returnMsg.setReturnCode(ReturnCode.ERROR);
-    	}
-    	
+    		    returnMsg.setReturnCode(ReturnCode.OK);
+   	      }
+    	    catch (Exception ex) {
+    		    returnMsg.setReturnCode(ReturnCode.ERROR);
+    	    }
         return returnMsg;
 	}
 	
-	
-	
-	
+
+	//download 
 	  public ReturnMsg MapFileurl(int siteId){
-		  
 		  ReturnMsg returnMsg = new ReturnMsg();
-		  
 		  try {
-	          
 	           Boolean exist = mapFileMapper.checkExist(siteId);
 	          if(exist == true) {
 
 	                   String fileName =  mapFileMapper.getfileName(siteId);
-	                   String downloadUrl  = "http://127.0.0.1:8080/Map/download/"+fileName;
-	                   
+	                   String downloadUrl  = "http://127.0.0.1:8080/Map/download/"+fileName;	                   
 //	                   returnMsg = new ReturnMsg(ReturnCode.OK, downloadUrl);
 	                   returnMsg.setData(downloadUrl);
 	            }
 	          else {
 	             returnMsg.setReturnCode(ReturnCode.NO_DATA);
-	          }
-	           
+	          }   
 	        }catch(Exception ex) {
 	           returnMsg.setReturnCode(ReturnCode.ERROR);
 	        }
-	        return returnMsg;
-	        
-		  
+	        return returnMsg;  
 	  }
-
-
-
 }
